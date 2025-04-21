0f956dfa5c2402274a3d350ae1252dd2f7345a96
Jorge Lucas <jorgelucastcoronado@gmail.com>
2025-04-20 23:47:52 -0300

    minor adjust in iframe

--- src/app/components/movie/MovieTrailer.tsx
+++ src/app/components/movie/MovieTrailer.tsx
@@ -5,12 +5,18 @@ const MovieTrailer = ({
   trailer?: string;
   title: string;
 }) => {
+  const idFromTrailer = trailer?.split("=")[1];
+
   return (
     <div className="relative flex flex-col w-full max-w-full m-8 bg-[#121113] text-white p-6 gap-6 rounded-xl shadow-xl z-10 overflow-hidden">
       <p className="text-3xl font-bold">{title}</p>
       <div>
         {trailer?.includes("youtube") && (
-          <iframe width="100%" height="556" src={trailer}></iframe>
+          <iframe
+            width="100%"
+            height="556"
+            src={`https://www.youtube.com/embed/${idFromTrailer}`}
+          ></iframe>
         )}
         {!trailer?.includes("youtube") && (
           <div className="flex justify-center items-center">
